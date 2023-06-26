a6067e9fb9f17104008b0a4f5be3f4cba2fa3a44
Eduard Ispolatov <u281094@gmail.com>
2023-06-26 21:16:10 +0300

    change server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./dist'));
+app.use("/", express.static(__dirname + "/dist"));
+app.use("/profile", express.static(__dirname + "/dist"));
+app.use("/chats", express.static(__dirname + "/dist"));
+app.use("/login", express.static(__dirname + "/dist"));
+app.use("/registration", express.static(__dirname + "/dist"));
 
-app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
-}); 
+app.listen(process.env.PORT, () => {
+    console.log('Server started')
+});
